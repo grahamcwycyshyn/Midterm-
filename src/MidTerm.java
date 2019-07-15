6dda178cb029cb2a2db25f448ebb0d62674c7ed0
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-07-15 10:27:54 -0400

    third line

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -5,6 +5,7 @@ public class MidTerm {
 				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("They were very clever monkeys indeed, "
 							+ "known for their ingenious teamwork.");
+		System.out.println("One fateful day, the nefarious Tai-Lung came to their peaceful land.");
 		
 	}
 }
