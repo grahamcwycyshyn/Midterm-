90bab50e030ce25bc7a17c121871d78f200e5715
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-07-15 10:32:03 -0400

    fifth line

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -7,5 +7,6 @@ public class MidTerm {
 							+ "known for their ingenious teamwork.");
 		System.out.println("One fateful day, the nefarious Tai-Lung came to their peaceful land.");
 		System.out.println("The monkeys were unperterbed by his evil presence, as they had trust in the power in numbers.");
+		System.out.println("But woe, Tai-Lung had come with his terrible army of scorpions!");
 	}
 }
