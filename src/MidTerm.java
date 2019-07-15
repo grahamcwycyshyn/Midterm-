11cfd243eb107e5b2224f7c685ba02f76cccd9c9
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-07-15 11:18:52 -0400

    bla bla

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -8,6 +8,6 @@ public class MidTerm {
 		System.out.println("The students were unperterbed by his evil presence, as they had trust in the power in numbers.");
 		System.out.println("But woe, Tai-Lung had come with his terrible army of scorpions!");
 		System.out.println("STING! ZING! The scorpoins attacked.");
-		System.out.println("The students were frightened! They had only one choice- name a warrior to read the Dragon Scroll and learn the ancient secrets of Kung-Fu!");
+		System.out.println("The students were frightened. They had only one choice- name a warrior to read the Dragon Scroll and learn the ancient secrets of Kung-Fu!");
 	}
 }
