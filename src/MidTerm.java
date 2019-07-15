4dc1c20199df3135dc1c09a35a07b26f1d66c314
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-07-15 11:06:33 -0400

    error

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -1,14 +1,13 @@
 
 public class MidTerm {
 	public static void main(String[] args) {
-		System.out.println(
-				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-		System.out.println("They were very clever monkeys indeed, "
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty band of ninja students.");
+		System.out.println("They were very clever students indeed, "
 							+ "known for their ingenious teamwork.");
 		System.out.println("One fateful day, the nefarious Tai-Lung came to their peaceful land.");
-		System.out.println("The monkeys were unperterbed by his evil presence, as they had trust in the power in numbers.");
+		System.out.println("The students were unperterbed by his evil presence, as they had trust in the power in numbers.");
 		System.out.println("But woe, Tai-Lung had come with his terrible army of scorpions!");
 		System.out.println("STING! ZING! The scorpoins attacked.");
-		System.out.println("The monkeys were frightened! They had only one choice- name a warrior to read the Dragon Scroll and learn the ancient secrets of Kung-Fu!");
+		System.out.println("The students were frightened! They had only one choice- name a warrior to read the Dragon Scroll and learn the ancient secrets of Kung-Fu!");
 	}
 }
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+	public static void main(String[] args) {
+		int[] arr = new int[3];
+		arr[0] = 2;
+		System.out.println(arr[0]);
+		
+	}
+}
